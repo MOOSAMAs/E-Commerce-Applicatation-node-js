b0b828ba8050202abfaa89be6ea0c29e2dc089d3
Mohamed Osama <medoosos129@gmail.com>
2024-09-20 08:37:45 +0300

    add apiFeatures class for paginate, fileration and more

--- src/modules/products/product.controller.js
+++ src/modules/products/product.controller.js
@@ -3,50 +3,38 @@ import { catchError } from "../../middleware/errorHandle.js";
 import { handleError } from "../../utils/customError.js";
 import { deleteOne } from "../handlers/factore.handler.js";
 import { productModel } from "../../../databases/models/product.model.js";
+import { apiFeatures} from "../../utils/apiFeatures.js";
 
-const addProduct = catchError(async (req , res , next)=>{
-    req.body.slug = slugify(req.body.title)
-    const result = new productModel(req.body)
-    await result.save()
-    res.status(201).json({message:'Product Added' , result})
-})
+const addProduct = catchError(async (req, res, next) => {
+  req.body.slug = slugify(req.body.title);
+  const result = new productModel(req.body);
+  await result.save();
+  res.status(201).json({ message: "Product Added", result });
+});
 
-const allProducts = catchError(async(req , res , next)=>{
-    let page = req.query.page*1 || 1
-    if(page <= 0 ) page = 1
-    const skip = (page-1) * 5
-    let filter = req.query
-    let exclude = ['page' , 'sort' , 'field']
-    exclude.forEach((q)=>{
-        delete filter[q]
-    })
-    filter = filter.stringfy(filter)
-    filter = filter.replace(/\b(gt | gte | lt | lte)\b/g , match=> `$${match}`)
-    filter = filter.parse(filter)
-    const result = await productModel.find({filter}).skip(skip).limit(5)
-    res.status(201).json({message:'all Product' , page , result })
-})
+const allProducts = catchError(async (req, res, next) => {
+  let features = new apiFeatures(productModel.find() , req.query)
+  .paginate().search().sort().filter()
+  const result = await features.mongooseQuery;
+  res.status(201).json({ message: "all Product", page:features.page, result });
+});
 
-const oneProduct = catchError(async(req , res , next)=>{
-    const {id} = req.params
-    const result = await productModel.findById(id)
-    res.status(201).json({message:'Specific Product' , result})
-})
+const oneProduct = catchError(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await productModel.findById(id);
+  res.status(201).json({ message: "Specific Product", result });
+});
 
-const updateProduct = catchError(async(req , res , next)=>{
-    const {id} = req.params
-    if(req.body.slug) req.body.slug = slugify(req.body.title)
-    const result = await productModel.findByIdAndUpdate(id , req.body , {new:true})
-    !result && next(new handleError('Product Not Found' , 401))
-    result && res.status(201).json({message:'Updated Successfully}' , result})
-})
+const updateProduct = catchError(async (req, res, next) => {
+  const { id } = req.params;
+  if (req.body.slug) req.body.slug = slugify(req.body.title);
+  const result = await productModel.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  !result && next(new handleError("Product Not Found", 401));
+  result && res.status(201).json({ message: "Updated Successfully}", result });
+});
 
-const deleteProduct = deleteOne(productModel)
+const deleteProduct = deleteOne(productModel);
 
-export {
-    addProduct,
-    allProducts,
-    oneProduct,
-    updateProduct,
-    deleteProduct
-}
+export { addProduct, allProducts, oneProduct, updateProduct, deleteProduct };
---
+++ src/utils/apiFeatures.js
@@ -0,0 +1,66 @@
+export class apiFeatures {
+  constructor(mongooseQuery, queryString) {
+    this.mongooseQuery = mongooseQuery;
+    this.queryString = queryString;
+  }
+
+  paginate() {
+    let page = this.queryString.page * 1 || 1;
+    if (page <= 0) page = 1;
+    const skip = (page - 1) * 5;
+    this.page = page;
+    this.mongooseQuery.skip(skip).limit(5)
+    return this;
+  }
+
+  filter(){
+    let filter = { ...this.queryString };
+  let exclude = ["page", "sort", "field" , 'search'];
+  exclude.forEach((q) => {
+    delete filter[q];
+  });
+  filter = JSON.stringify(filter);
+  filter = filter.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
+  filter = JSON.parse(filter);
+  this.mongooseQuery.find(filter)
+  return this;
+  }
+
+  sort(){
+    if (this.queryString.sort) {
+        let sortBy = this.queryString.sort.split(",").join(" ");
+        this.mongooseQuery.sort(sortBy);
+      }
+      return this;
+  }
+
+  search(){
+    if (this.queryString.keyword) {
+        this.mongooseQuery.find({
+          $or: [
+            {
+              title: {
+                $regex: this.queryString.keyword,
+                $options: 'i',
+              },
+            },
+            {
+              description: {
+                $regex: this.queryString.keyword,
+                $options: 'i',
+              },
+            },
+          ],
+        });
+      }
+      return this
+  }
+
+  fields(){
+    if (this.queryString.fields) {
+        let fields = req.query.fields.split(",").join(" ");
+        this.mongooseQuery.select(fields);
+      }
+      return this
+  }
+}
