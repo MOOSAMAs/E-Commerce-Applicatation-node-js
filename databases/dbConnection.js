721065448db93451c0fce9bbd512fb4b5a87998e
Mohamed Osama <medoosos129@gmail.com>
2024-10-13 06:47:47 +0300

    -

--- databases/dbConnection.js
+++ databases/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export const dbConnection = ()=>{
-    mongoose.connect(process.env.DATABASE_URL).then(()=>{
+    mongoose.connect('mongodb+srv://mohamed178:Mohamed@cluster0.dbycv4m.mongodb.net/E-CommerceApp').then(()=>{
         console.log('Database Connected..');
     }).catch((err)=>{
         console.log('ERROR' , err);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
 import morgan from 'morgan'
+import cors from 'cors'
 import { dbConnection } from './databases/dbConnection.js'
 import { init } from './src/modules/index.routes.js'
 
@@ -8,10 +9,11 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 app.use(express.static('uploads'))
 app.use(morgan('dev'))
 init(app)
 
 dbConnection()
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port , () => console.log(`Example app listening on port ${port}!`))
